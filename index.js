c266a254f48af15ed5ed415af58a9b660546684f
unknown <ashishkumarnath9@gmail.com>
2023-08-28 16:06:46 +0530

    added new route bleach

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const express = require("express")
+const express=require("express")
 
 const app = express()
 
 
 
-app.get("/", (req, res) => {
+app.get("/",(req,res)=>{
 
     let animeName = "JoJo's Bizzare Adventure"
     res.send(`<h1>${animeName}</h1>`)
+})
 
+app.get("/bleach",(req,res)=>{
+
+    let name = "bleach"
+    res.send(`<h1>${name}</h1>`)
 })
 
 
-app.listen(3000, () => {
+app.listen(3000,()=>{
     console.log("listening on port 3000")
 })
