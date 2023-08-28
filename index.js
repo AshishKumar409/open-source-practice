fe1babcc0ac26317b0983f2b9114d7c046b8eb53
Ashish Kumar Nath <ashishkumarnath9@gmail.com>
2023-08-28 21:36:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  let animeName = "JJBA";
+  let animeName = "JOJO'S BIZZARE ADVENTURE";
   res.send(`<h1>${animeName}</h1>`);
 });
 
