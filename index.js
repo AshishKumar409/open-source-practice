407a63e6fbc5317ea73058c9c0752f3b66c899ef
unknown <ashishkumarnath9@gmail.com>
2023-08-28 20:21:58 +0530

    prerrier applied :(

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-const express=require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
+app.get("/", (req, res) => {
+  let animeName = "JoJo's Bizzare Adventure";
+  res.send(`<h1>${animeName}</h1>`);
+});
 
+app.get("/bleach", (req, res) => {
+  let name = "bleach";
+  res.send(`<h1>${name}</h1>`);
+});
 
-app.get("/",(req,res)=>{
-
-    let animeName = "JoJo's Bizzare Adventure"
-    res.send(`<h1>${animeName}</h1>`)
-})
-
-app.get("/bleach",(req,res)=>{
-
-    let name = "bleach"
-    res.send(`<h1>${name}</h1>`)
-})
-
-
-app.listen(3000,()=>{
-    console.log("listening on port 3000")
-})
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
